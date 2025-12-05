65e62070bc1a286d3f71a7311335a451f316c21b
GalGelfman <galgelfman@gmail.com>
2025-12-05 23:22:34 +0200

    Add a coordinator between the two lists of Missions, initialize the model and view models in main using Provider package

---
+++ Frontend/hamal_transport_app/lib/ViewModels/missions_coordinator_view_model.dart
@@ -0,0 +1,37 @@
+import 'package:hamal_transport_app/Models/mission.dart';
+import 'package:hamal_transport_app/ViewModels/available_missions_view_model.dart';
+import 'package:hamal_transport_app/ViewModels/mission_view_model.dart';
+import 'package:hamal_transport_app/ViewModels/my_missions_view_model.dart';
+
+class MissionsCoordinatorViewModel {
+  final MyMissionsViewModel myMissionsVM;
+  final AvailableMissionsViewModel availableMissionsVM;
+  final MissionViewModel missionVM;
+
+  MissionsCoordinatorViewModel({
+    required this.myMissionsVM,
+    required this.availableMissionsVM,
+    required this.missionVM,
+  });
+
+  /// Move mission from Available → My Missions
+  void takeMission(Mission mission) {
+    missionVM.updateStatus(mission, 'chosen');
+    availableMissionsVM.remove(mission);
+    myMissionsVM.add(mission);
+  }
+
+  /// Move mission from My → Available Missions
+  void abandonMission(Mission mission) {
+    myMissionsVM.remove(mission);
+    availableMissionsVM.add(mission);
+  }
+
+  void updateStatus(Mission mission, String newStatus) {
+    if (newStatus == 'cancelled') {
+      abandonMission(mission);
+      newStatus = 'available';
+    }
+    missionVM.updateStatus(mission, newStatus);
+  }
+}
