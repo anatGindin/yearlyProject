c2fa07bbf3e1ce09c7262fc1c60efe4c284ffcf7
Anat Gindin <120976073+anatGindin@users.noreply.github.com>
2025-12-30 17:03:58 +0200

    added mock_data.json in Backend, changed status update and added status change checks

--- Backend/routers/missions.py
+++ Backend/routers/missions.py
@@ -8,7 +8,7 @@ from app.models import Mission
 router = APIRouter(prefix="/missions", tags=["missions"])
 
 # TEMP storage (replace with DB later)
-file_path = "/Frontend/hamal_transport_app/assets/mock_data.json"
+file_path = "/Backend/mock_data.json"
 
 if os.path.exists(file_path):
     with open(file_path, "r") as f:
@@ -27,14 +27,29 @@ def create_mission(new_mission: Mission):
     save_missions()
     return new_mission
 
-@router.put("/{mission_id}", response_model=Mission)
+@router.put("/{mission_id}/{status}", response_model=Mission)
+def update_mission_status(mission_id: str, status: str):
+    for mission in missions:
+        if mission.id == mission_id:
+            # check if status is valid
+            if status not in ["pending", "in_progress", "completed", "cancelled"]:
+                raise HTTPException(status_code=400, detail="Invalid status value")
+            if(mission.status == "pending" and status != "in_progress"):
+                raise HTTPException(status_code=400, detail="Invalid status transition from pending")
+            if(mission.status == "in_progress" and status == "pending"):
+                raise HTTPException(status_code=400, detail="Invalid status transition from in_progress")
+            mission.status = status
+            save_missions()
+            return mission
+    raise HTTPException(status_code=404, detail="Mission not found")
+'''@router.put("/{mission_id}", response_model=Mission)
 def update_mission(mission_id: str, updated_mission: Mission):
     for index, mission in enumerate(missions):
         if mission.id == mission_id:
             missions[index] = updated_mission
             save_missions()
             return updated_mission
-    raise HTTPException(status_code=404, detail="Mission not found")
+    raise HTTPException(status_code=404, detail="Mission not found")'''
 
 @router.delete("/{mission_id}", status_code=204)
 def delete_mission(mission_id: str):
