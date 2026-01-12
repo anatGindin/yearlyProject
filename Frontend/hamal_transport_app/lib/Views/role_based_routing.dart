61383b6f6ef3ac636d40349c3c1ce62409ff99f2
dvasha <dvasha@campus.technion.ac.il>
2026-01-12 18:06:33 +0200

    fix mapview

--- Frontend/hamal_transport_app/lib/Views/role_based_routing.dart
+++ Frontend/hamal_transport_app/lib/Views/role_based_routing.dart
@@ -2,9 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:hamal_transport_app/Models/mission_list_type.dart';
 import 'package:hamal_transport_app/Models/missions_model.dart';
 import 'package:hamal_transport_app/Services/missions_repository.dart';
-import 'package:hamal_transport_app/ViewModels/driver_map_view_model.dart';
+import 'package:hamal_transport_app/ViewModels/map_view_model.dart';
 import 'package:hamal_transport_app/ViewModels/missions_list_view_model.dart';
-import 'package:hamal_transport_app/Views/driver_map_view.dart';
+import 'package:hamal_transport_app/Views/map_view.dart';
 import 'package:hamal_transport_app/Views/navigation_bar_wrapper.dart';
 import 'package:hamal_transport_app/Views/missions_tabs_page.dart';
 import 'package:hamal_transport_app/Views/under_construction_page.dart';
@@ -22,7 +22,7 @@ Widget getDestinationForRole(UserRole role) {
   switch (role) {
     case UserRole.driver:
       return ChangeNotifierProvider(
-        create: (_) => DriverMapViewModel(),
+        create: (_) => MapViewModel(),
         child: Builder(
           builder: (context) {
             final l10n = AppLocalizations.of(context)!;
@@ -55,11 +55,10 @@ Widget getDestinationForRole(UserRole role) {
                 ),
                 NavBarDestination(
                   pageType: NavBarPageType.mapView,
-                  page: const DriverMapView(),
-                  onEnter: () =>
-                      context.read<DriverMapViewModel>().initLocation(),
+                  page: const MapView(),
+                  onEnter: () => context.read<MapViewModel>().initLocation(),
                   onExit: () =>
-                      context.read<DriverMapViewModel>().stopLocationUpdates(),
+                      context.read<MapViewModel>().stopLocationUpdates(),
                 ),
                 const NavBarDestination(
                   pageType: NavBarPageType.profile,
@@ -73,7 +72,7 @@ Widget getDestinationForRole(UserRole role) {
     case UserRole.logistics:
     case UserRole.admin:
       return ChangeNotifierProvider(
-        create: (_) => DriverMapViewModel(),
+        create: (_) => MapViewModel(),
         child: Builder(
           builder: (context) {
             final l10n = AppLocalizations.of(context)!;
@@ -145,11 +144,10 @@ Widget getDestinationForRole(UserRole role) {
                 ),
                 NavBarDestination(
                   pageType: NavBarPageType.mapView,
-                  page: const DriverMapView(),
-                  onEnter: () =>
-                      context.read<DriverMapViewModel>().initLocation(),
+                  page: const MapView(),
+                  onEnter: () => context.read<MapViewModel>().initLocation(),
                   onExit: () =>
-                      context.read<DriverMapViewModel>().stopLocationUpdates(),
+                      context.read<MapViewModel>().stopLocationUpdates(),
                 ),
                 const NavBarDestination(
                   pageType: NavBarPageType.profile,
