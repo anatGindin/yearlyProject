7460401c239618db11dac0f87df8427860ac2559
dvasha <dvasha@campus.technion.ac.il>
2026-01-13 14:36:20 +0200

    rm changes to role based routing

--- Frontend/hamal_transport_app/lib/Views/role_based_routing.dart
+++ Frontend/hamal_transport_app/lib/Views/role_based_routing.dart
@@ -7,11 +7,9 @@ import 'package:hamal_transport_app/ViewModels/missions_list_view_model.dart';
 import 'package:hamal_transport_app/Views/map_view.dart';
 import 'package:hamal_transport_app/Views/navigation_bar_wrapper.dart';
 import 'package:hamal_transport_app/Views/missions_tabs_page.dart';
-import 'package:hamal_transport_app/Views/under_construction_page.dart';
 import 'package:hamal_transport_app/Views/user_profile_page.dart';
 import 'package:hamal_transport_app/l10n/app_localizations.dart';
 import 'package:provider/provider.dart';
-import '../Models/mission.dart';
 import '../Models/user_profile.dart';
 //
 
@@ -75,79 +73,17 @@ Widget getDestinationForRole(UserRole role) {
         create: (_) => MapViewModel(),
         child: Builder(
           builder: (context) {
-            final l10n = AppLocalizations.of(context)!;
-            final repository = context.read<MissionsRepository>();
             return NavigationBarWrapper(
               allDestinations: [
-                NavBarDestination(
-                  pageType: NavBarPageType.missions,
-                  page: MissionsTabsPage(
-                    tabs: [
-                      MissionTabConfig(
-                        title: l10n.readyForPickUp,
-                        icon: Icons.assignment_turned_in,
-                        color: MissionStatus.available.statusColor,
-                        viewModel: MissionsListViewModel(
-                          repository: repository,
-                          type: MissionListType.availableMissions,
-                          allowedFilterOptions: const [],
-                        ),
-                      ),
-                      MissionTabConfig(
-                        title: l10n.readyForPickUpMissions,
-                        icon: Icons.heart_broken,
-                        color: MissionStatus.assigned.statusColor,
-                        viewModel: MissionsListViewModel(
-                          repository: repository,
-                          type: MissionListType.assignedMissions,
-                          allowedFilterOptions: const [],
-                        ),
-                      ),
-                      MissionTabConfig(
-                        title: l10n.pickedUpMissions,
-                        icon: Icons.favorite,
-                        color: MissionStatus.pickedUp.statusColor,
-
-                        viewModel: MissionsListViewModel(
-                          repository: repository,
-                          type: MissionListType.pickedUpMissions,
-                          allowedFilterOptions: const [],
-                        ),
-                      ),
-                      MissionTabConfig(
-                        title: l10n.cancelledMissions,
-                        icon: Icons.sentiment_very_dissatisfied,
-                        color: MissionStatus.cancelled.statusColor,
-
-                        viewModel: MissionsListViewModel(
-                          repository: repository,
-                          type: MissionListType.cancelledMissions,
-                          allowedFilterOptions: const [],
-                        ),
-                      ),
-                      MissionTabConfig(
-                        title: l10n.deliveredMissions,
-                        icon: Icons.check_circle,
-                        color: MissionStatus.delivered.statusColor,
-                        viewModel: MissionsListViewModel(
-                          repository: repository,
-                          type: MissionListType.deliveredMissions,
-                          allowedFilterOptions: const [],
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-                const NavBarDestination(
-                  pageType: NavBarPageType.driverList,
-                  page: UnderConstructionPage(),
-                ),
+                //TODO: add more pages for logistics
                 NavBarDestination(
                   pageType: NavBarPageType.mapView,
                   page: const MapView(),
-                  onEnter: () => context.read<MapViewModel>().initLocation(),
+                  // MARK: if we introduce location services on the map page for logisticians, uncomment the onEnter and onExit methods
+                  onEnter: () =>
+                      {}, // context.read<MapViewModel>().initLocation(),
                   onExit: () =>
-                      context.read<MapViewModel>().stopLocationUpdates(),
+                      {}, // context.read<MapViewModel>().stopLocationUpdates(),
                 ),
                 const NavBarDestination(
                   pageType: NavBarPageType.profile,
