68270615069ecaba0416e498771eb873ea7d268c
GalGelfman <galgelfman@gmail.com>
2026-01-13 22:40:24 +0200

    Add phone book v and vm

---
+++ Frontend/hamal_transport_app/lib/ViewModels/driver_phone_book_view_model.dart
@@ -0,0 +1,133 @@
+import 'package:flutter/material.dart';
+import 'package:hamal_transport_app/Services/authentication_service.dart';
+import 'package:hamal_transport_app/l10n/app_localizations.dart';
+import '../Models/user_profile.dart';
+
+class DriverPhoneBookViewModel extends ChangeNotifier {
+  final List<UserProfile> _allUsers = List<UserProfile>.empty(growable: true);
+  CarType? _carTypeFilter;
+  List<CarType?> allowedFilterOptions = [null, ...CarType.values];
+
+  String _query = '';
+  bool _ascending = true;
+
+  DriverPhoneBookViewModel() {
+    _getUsers();
+  }
+
+  String get query => _query;
+  bool get ascending => _ascending;
+
+  List<UserProfile> get drivers {
+    final filtered = _allUsers
+        .where((u) => u.role == UserRole.driver)
+        .where(
+          (u) => _carTypeFilter != null
+              ? u.driverProfile!.carType == _carTypeFilter
+              : true,
+        )
+        .where(
+          (u) =>
+              u.name.toLowerCase().contains(_query.toLowerCase()) ||
+              u.phone.contains(_query),
+        )
+        .toList();
+
+    filtered.sort(
+      (a, b) =>
+          _ascending ? a.name.compareTo(b.name) : b.name.compareTo(a.name),
+    );
+
+    return filtered;
+  }
+
+  CarType? get filter => _carTypeFilter;
+
+  void updateQuery(String value) {
+    _query = value;
+    notifyListeners();
+  }
+
+  void toggleSortOrder() {
+    _ascending = !_ascending;
+    notifyListeners();
+  }
+
+  Future<void> _getUsers() async {
+    //TODO: get users from firebase or mock data
+    final authService = AuthenticationService();
+    UserProfile? driverProfile;
+    try {
+      driverProfile = await authService.getUserProfileByUid(
+        "I9ivZ6H8pYWoDkeb2wybbKXgxWE2",
+      );
+    } catch (e) {
+      driverProfile = null;
+    } finally {
+      if (driverProfile != null) {
+        _allUsers.add(driverProfile);
+      }
+      notifyListeners();
+    }
+    _allUsers.add(
+      UserProfile(
+        uid: "uid1",
+        email: 'uid1@email.com',
+        name: 'user 1',
+        phone: '0501234567',
+        role: UserRole.driver,
+        driverProfile: DriverProfile(carType: CarType.private),
+      ),
+    );
+    _allUsers.add(
+      UserProfile(
+        uid: "uid2",
+        email: 'uid2@email.com',
+        name: 'user 2',
+        phone: '0501234568',
+        role: UserRole.driver,
+        driverProfile: DriverProfile(carType: CarType.truck),
+      ),
+    );
+    _allUsers.add(
+      UserProfile(
+        uid: "uid3",
+        email: 'uid3@email.com',
+        name: 'user 3',
+        phone: '0501234569',
+        role: UserRole.driver,
+        driverProfile: DriverProfile(carType: CarType.private),
+      ),
+    );
+    _allUsers.add(
+      UserProfile(
+        uid: "uid4",
+        email: 'uid4@email.com',
+        name: 'user 4',
+        phone: '0501234560',
+        role: UserRole.driver,
+        driverProfile: DriverProfile(carType: CarType.trailer),
+      ),
+    );
+  }
+
+  String getFilterBy(AppLocalizations l10n, CarType? option) {
+    switch (option) {
+      case null:
+        return l10n.allCarTypes;
+      case CarType.private:
+        return l10n.privateOnly;
+      case CarType.trailer:
+        return l10n.trailerOnly;
+      case CarType.pickupTruck:
+        return l10n.pickupOnly;
+      case CarType.truck:
+        return l10n.truckOnly;
+    }
+  }
+
+  void filterBy(CarType? option) {
+    _carTypeFilter = option;
+    notifyListeners();
+  }
+}
---
+++ Frontend/hamal_transport_app/lib/Views/driver_phone_book_screen.dart
@@ -0,0 +1,95 @@
+import 'package:flutter/material.dart';
+import 'package:hamal_transport_app/Views/Widgets/list_action_button.dart';
+import 'package:hamal_transport_app/l10n/app_localizations.dart';
+import 'package:provider/provider.dart';
+import '../../ViewModels/driver_phone_book_view_model.dart';
+import 'Widgets/driver_list_view.dart';
+
+class DriverPhoneBookScreen extends StatelessWidget {
+  const DriverPhoneBookScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider(
+      create: (_) => DriverPhoneBookViewModel(),
+      child: const _DriverPhoneBookView(),
+    );
+  }
+}
+
+class _DriverPhoneBookView extends StatelessWidget {
+  const _DriverPhoneBookView();
+
+  @override
+  Widget build(BuildContext context) {
+    final vm = context.watch<DriverPhoneBookViewModel>();
+    final l10n = AppLocalizations.of(context)!;
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(l10n.drivers),
+        backgroundColor: Theme.of(context).colorScheme.surface,
+        surfaceTintColor: Colors.transparent,
+        scrolledUnderElevation: 0,
+      ),
+      body: Column(
+        children: [
+          Row(
+            children: [
+              Expanded(
+                child: ListActionButton(
+                  icon: Icons.filter_alt,
+                  label: vm.getFilterBy(l10n, vm.filter),
+                  onPressed: () => _showFilterOptions(context),
+                  height: 0.1,
+                  width: 0.1,
+                ),
+              ),
+            ],
+          ),
+
+          /// Search
+          Padding(
+            padding: const EdgeInsets.all(16),
+            child: TextField(
+              onChanged: vm.updateQuery,
+              decoration: InputDecoration(
+                prefixIcon: const Icon(Icons.search),
+                hintText: l10n.searchDriver,
+              ),
+            ),
+          ),
+          Expanded(
+            child: SingleChildScrollView(
+              child:
+                  /// List
+                  DriverListView(drivers: vm.drivers),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  void _showFilterOptions(BuildContext context) {
+    final viewModel = context.read<DriverPhoneBookViewModel>();
+    final l10n = AppLocalizations.of(context)!;
+    showModalBottomSheet(
+      context: context,
+      builder: (_) => Column(
+        mainAxisSize: MainAxisSize.min,
+        children: viewModel.allowedFilterOptions
+            .map(
+              (option) => ListTile(
+                title: Text(viewModel.getFilterBy(l10n, option)),
+                onTap: () {
+                  viewModel.filterBy(option);
+                  Navigator.pop(context);
+                },
+              ),
+            )
+            .toList(),
+      ),
+    );
+  }
+}
