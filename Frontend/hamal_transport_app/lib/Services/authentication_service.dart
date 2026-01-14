f739b107b21e2c3e6400ef1e4b03ef799bd429da
GalGelfman <galgelfman@gmail.com>
2026-01-14 12:42:38 +0200

    Remove unused import

--- Frontend/hamal_transport_app/lib/Services/authentication_service.dart
+++ Frontend/hamal_transport_app/lib/Services/authentication_service.dart
@@ -1,5 +1,4 @@
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:firebase_database/firebase_database.dart';
 import 'package:hamal_transport_app/Models/user_profile.dart';
