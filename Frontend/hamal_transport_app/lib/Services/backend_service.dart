4a406e4a82089361b972585613a24f840fecfb52
Anat Gindin <120976073+anatGindin@users.noreply.github.com>
2026-01-14 09:01:39 +0200

    fixed url

--- Frontend/hamal_transport_app/lib/Services/backend_service.dart
+++ Frontend/hamal_transport_app/lib/Services/backend_service.dart
@@ -1,6 +1,6 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
-import '../models/mission.dart';
+import '../Models/mission.dart';
 
 class MissionService {
   final String baseUrl;
