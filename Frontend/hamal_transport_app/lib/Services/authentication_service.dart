bca52b0a396b7bc5a630e966601aa8243b3f1e7f
Omri Hamani <omri.hamani@gmail.com>
2026-05-21 16:19:47 +0300

    remove unued import

--- Frontend/hamal_transport_app/lib/Services/authentication_service.dart
+++ Frontend/hamal_transport_app/lib/Services/authentication_service.dart
@@ -1,7 +1,6 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/foundation.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 import 'package:hamal_transport_app/Services/persistant_storagte_service.dart';
 import 'package:firebase_database/firebase_database.dart';
 import 'package:hamal_transport_app/Models/user_profile.dart';
