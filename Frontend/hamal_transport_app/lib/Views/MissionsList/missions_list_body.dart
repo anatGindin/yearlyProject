27e3e3f8523370a7be941cc9435f0476f5a687c5
GalGelfman <galgelfman@gmail.com>
2026-05-26 23:47:37 +0300

    add TODO

--- Frontend/hamal_transport_app/lib/Views/MissionsList/missions_list_body.dart
+++ Frontend/hamal_transport_app/lib/Views/MissionsList/missions_list_body.dart
@@ -89,6 +89,7 @@ class MissionsListBody extends StatelessWidget {
                     ),
                     const SizedBox(height: 30),
                     viewModel.isLoading
+                        // TODO: change indicator with skeleton
                         ? const Center(child: CircularProgressIndicator())
                         : MissionListView(missions: viewModel.missions),
                     SizedBox(height: MediaQuery.of(context).size.height * 0.2),
